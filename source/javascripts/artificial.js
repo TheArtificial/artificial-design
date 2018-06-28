e747ec4f87f22e4e512e2cc57db21a9d205f80eb
Hans Gerwitz <hans@gerwitz.com>
2018-06-28 21:53:01 +0200

    panel scrolling simplification

--- source/javascripts/artificial.js
+++ source/javascripts/artificial.js
@@ -1,163 +1,96 @@
-// this binds navigation items to items within the body
-// thanks https://jsfiddle.net/mekwall/up4nu/
+// here we watch the scrolling panels and smoothly update colors
 function scrollspy(selector, heightOffset, callback) {
-	var	scrollTicking = false;
-	var lastId,
-	    menu = $(selector),
-	    menuItems = menu.find("a"),
-	    scrollItems = menuItems.map(function(){
-	      var item = $($(this).attr("href"));
-	      if (item.length) { return item; }
-	    });
+		var	scrollTicking = false;
+		var scrollItems = $(selector);
 
-	// disabled becuase it got janky
-	// Bind click handler to menu items so we can get a fancy scroll animation
-	// menuItems.click(function(e){
-	//   var href = $(this).attr("href"),
-	// 			target = $(href),
-	//       offsetTop = href === "#" ? 0 : target.offset().top+1;
-  //   $("html, body").stop().animate({
-  //     scrollTop: (offsetTop - heightOffset)
-  //   }, 600);
-  //   e.preventDefault();
-	// });
+		// Do all the things
+		function scrollUpdate() {
+			// Get container scroll position
+			var fromTop = $(this).scrollTop();
 
-	// Do all the things
-	function scrollUpdate() {
-	  // Get container scroll position
-	  var fromTop = $(this).scrollTop();
+			// Test every scroll item to see if it's in view
+			var onScreen = scrollItems.map(function(){
+				if ($(this).offset().top <= (fromTop + heightOffset))
+					return this;
+			});
 
-		// Test every scroll item to see if it's in view
-	  var onScreen = scrollItems.map(function(){
-	    if ($(this).offset().top <= (fromTop + heightOffset))
-	      return this;
-	  });
-		// Treat the last one as current
-	  var current = onScreen[onScreen.length-1];
-	  var id = current && current.length ? current[0].id : "";
-		// var id = current[0].id;
+			// Treat the last one as current
+			var current = onScreen[onScreen.length-1];
 
-		if (id && (callback != 'undefined')) {
-			var	currentElement = $('#'+id),
-					offset = 0;
-			if (currentElement.offset()) {
-				offset = Math.min((fromTop - currentElement.offset().top) / (currentElement.height()),1.0);
+			if (current && (callback != 'undefined')) {
+					var	currentElement = $(current),
+							offset = 0;
+					if (currentElement.offset()) {
+							offset = Math.min((fromTop - currentElement.offset().top) / (currentElement.height()),1.0);
+					}
+					callback(currentElement, offset);
 			}
-			callback(currentElement, offset);
+
+				scrollTicking = false;
 		}
 
-	  if (lastId !== id) {
-			updatePagination(menuItems, id);
-			lastId = id;
-	  }
-
-		scrollTicking = false;
-	}
-
-	// For a good explanation of this, see http://www.html5rocks.com/en/tutorials/speed/animations/
-	function debounceScroll() {
-		if(!scrollTicking) {
-			requestAnimationFrame(scrollUpdate);
+		// For a good explanation of this, see http://www.html5rocks.com/en/tutorials/speed/animations/
+		function debounceScroll() {
+			if(!scrollTicking) {
+					requestAnimationFrame(scrollUpdate);
+			}
+			scrollTicking = true;
 		}
-		scrollTicking = true;
-	}
 
-	$(window).scroll(debounceScroll);
+		$(window).scroll(debounceScroll);
 
 }
 
-function updatePagination(menuItems, currentId) {
-	menuItems.parent().removeClass("active");
-	// find the item that is not .prev or .next and has the right href target
-	var currentMenuItem = menuItems.filter('a:not(.next,.prev)[href=\'#'+currentId+'\']');
-	currentMenuItem.parent().addClass('active');
-	var prevMenuItem = currentMenuItem.parent().prev().find('a:not(.prev)');
-	if (prevMenuItem.length > 0) {
-		menuItems.filter('.prev').attr('href',prevMenuItem.attr('href')).parent().removeClass('disabled');
-	} else {
-		// console.log("nothing prev", currentId);
-		menuItems.filter('.prev').removeAttr('href').removeAttr('href').parent().addClass('disabled');
-	}
-	var nextMenuItem = currentMenuItem.parent().next().find('a:not(.next)');
-	if (nextMenuItem.length > 0) {
-		menuItems.filter('.next').attr('href',nextMenuItem.attr('href')).parent().removeClass('disabled');
-	} else {
-		// console.log("nothing next", currentId);
-		menuItems.filter('.next').removeAttr('href').parent().addClass('disabled');
-	}
-}
 
 // this brings up search by typing
 function eversearch() {
-	// Detect keystroke and present search
-	var $searchField = $("#oncall .search .searchbox");
+		// Detect keystroke and present search
+		var $searchField = $("#oncall .search .searchbox");
 
-	function hideSearch(skipblur) {
-		$("#oncall").css("display","none");
-		if (!skipblur) {
-			$searchField.blur();
-		}
-	}
-
-	function showSearch() {
-		$("#oncall").css("display","block");
-		$searchField.focus();
-	}
-
-	$(window).keypress(function(e) {
-		if ($(e.target).is('input, textarea')) {
-			return;
+		function hideSearch(skipblur) {
+				$("#oncall").css("display","none");
+				if (!skipblur) {
+						$searchField.blur();
+				}
 		}
 
-		var code = e.charCode || e.which;
-
-		switch (code) {
-			case 8:
-			case 27:
-				// do nothing
-				break;
-			default:
-				showSearch();
+		function showSearch() {
+				$("#oncall").css("display","block");
+				$searchField.focus();
 		}
-	});
 
-	$searchField.keydown(function(e) {
-		var code = e.charCode || e.which;
+		$(window).keypress(function(e) {
+				if ($(e.target).is('input, textarea')) {
+						return;
+				}
 
-		// exit upon esc
-		if (code == 27) {
-	 		hideSearch();
-		}
-		// exit upon delete
-		if ((code == 8) && !$searchField.val()) {
-			hideSearch();
-		}
-	});
+				var code = e.charCode || e.which;
 
-	$searchField.blur(function(e) {
-		$searchField.val("");
-		hideSearch(true);
-	});
-}
-
-// this filters items
-function listfilter() {
-	$("select.filter").change(function() {
-		var selectedFilters = $.map($("select.filter").toArray(), function(e){
-        return $(e).val();
+				switch (code) {
+						case 8:
+						case 27:
+								// do nothing
+								break;
+						default:
+								showSearch();
+				}
 		});
-		var filteredFilters = selectedFilters.filter(function(filterItem) {
-    	if (filterItem == "all") {
-				return false;
-			}
-			else {
-				return true;
-			}
-    });
-		filteredFilters.unshift("div");
-		var filterSelector = filteredFilters.join(".");
 
-		$("div#animal-list .col-3").find("div").addClass("filter-off");
-    $("div#animal-list .col-3").find(filterSelector).removeClass("filter-off");
-	});
+		$searchField.keydown(function(e) {
+				var code = e.charCode || e.which;
+
+				// exit upon esc
+				if (code == 27) {
+						 hideSearch();
+				}
+				// exit upon delete
+				if ((code == 8) && !$searchField.val()) {
+						hideSearch();
+				}
+		});
+
+		$searchField.blur(function(e) {
+				$searchField.val("");
+				hideSearch(true);
+		});
 }
